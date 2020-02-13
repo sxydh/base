0f6e11c65a4c4c762c33f8a6453698e59241bde4
sxydh <sxydh@gmail.com>
2020-02-13 09:37:54 +0800

    build

--- java-main-test/basics/src/main/java/cn/net/bhe/basics/base/lambda/Main.java
+++ java-main-test/basics/src/main/java/cn/net/bhe/basics/base/lambda/Main.java
@@ -4,27 +4,18 @@ public class Main {
 
     /**
      * 用途一
-     * 简化匿名类书写，但匿名类继承的接口必须带@FunctionalInterface
+     * 简化匿名类书写
      */
     public static void
     // simplifiedAnonymous() 
     main(String[] args)
     {
-        FunctionalInterfac func = string -> System.out.println(string);
-        /* 以上相当于
-        new FunctionalInterfac() {
-            @Override
-            public void method(String arg) {
-                System.out.println(arg);
-            }
-        };
-        */
-        func.method("anonymous");
+        Interfac interfac = string -> System.out.println(string);
+        interfac.method("anonymous");
     }
 }
 
-@FunctionalInterface
-interface FunctionalInterfac {
+interface Interfac {
     public void method(String arg); // 默认方法
     public default Integer extendMethod(String arg) { // default方法子类可以选择性实现，这避免了接口扩展时子类无法通过编译的问题
         return Integer.parseInt(arg);

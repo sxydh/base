0ba98828cc737cb7084d9b8d854a89e300c4f3bd
sxydh <sxydh@gmail.com>
2020-03-07 17:57:00 +0800

    build

--- java-main-test/basics/src/main/java/cn/net/bhe/basics/proxy/cglib/App.java
+++ java-main-test/basics/src/main/java/cn/net/bhe/basics/proxy/cglib/App.java
@@ -6,6 +6,12 @@ import org.springframework.cglib.proxy.Enhancer;
 import org.springframework.cglib.proxy.MethodInterceptor;
 import org.springframework.cglib.proxy.MethodProxy;
 
+/**
+ * CGLIB(Code Generation Library)是一个基于ASM的字节码生成库
+ * 它允许我们在运行时对字节码进行修改和动态生成
+ * CGLIB通过继承方式实现代理
+ * 目标不能是final的
+ */
 public class App {
 
     public static void main(String[] args) {

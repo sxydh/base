d0af69d6759a9803bac65240b3ebe3328d2e9778
sxydh <sxydhgg@gmail.com>
2020-03-17 20:07:16 +0800

    build

--- java-main-test/introduction-to-algorithms/src/main/java/cn/net/bhe/introductiontoalgorithms/common/MaxSubArray.java
+++ java-main-test/introduction-to-algorithms/src/main/java/cn/net/bhe/introductiontoalgorithms/common/MaxSubArray.java
@@ -3,8 +3,8 @@ package cn.net.bhe.introductiontoalgorithms.common;
 public class MaxSubArray {
 
     public static
-//  void main(String[] args)
-    int greedy(int[] nums)
+//    void main(String[] args)
+    int dynamicProgramming(int[] nums) // 动态规划
     {
         if (nums.length == 1) return nums[0];
         int cursum = nums[0];
@@ -14,6 +14,51 @@ public class MaxSubArray {
             maxsum = Math.max(cursum, maxsum);
         }
         return maxsum;
+    }
+    
+    public static
+//    void main(String[] args)
+    int divideAndConquer(int[] nums) // 分治策略
+    {
+//        int[] nums = { 1, 2 };
+        int result = doDivideAndConquer(nums, 0, nums.length - 1);
+        return result;
+//        System.out.println(result);
+    }
+    
+    static int doDivideAndConquer(int[] nums, int start, int end) {
+        if (end == start) {
+            return nums[start];
+        }
+        int middle = (end - start) / 2 + start;
+        int lMax = doDivideAndConquer(nums, start, middle);
+        int rMax = doDivideAndConquer(nums, middle + 1, end);
+        int lMaxWithBorder = lMaxWithBorder(nums, start, middle);
+        int rMaxWithBorder = rMaxWithBorder(nums, middle + 1, end);
+        int max = Math.max(lMax, rMax);
+        return Math.max(max, lMaxWithBorder + rMaxWithBorder);
+    }
+    
+    static int lMaxWithBorder(int[] nums, int start, int end) {
+        int sum = nums[end];
+        int max = sum;
+        int i = end - 1;
+        while (i >= start) {
+            max = Math.max(max, sum += nums[i]);
+            i--;
+        }
+        return max;
+    }
+    
+    static int rMaxWithBorder(int[] nums, int start, int end) {
+        int sum = nums[start];
+        int max = sum;
+        int i = start + 1;
+        while (i <= end) {
+            max = Math.max(max, sum += nums[i]);
+            i++;
+        }
+        return max;
     }
     
 }

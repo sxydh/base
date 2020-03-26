f35c8ad3412b258d06466a14563f68c1e92f42fd
sxydh <sxydhgg@gmail.com>
2020-03-26 20:31:26 +0800

    build

--- java-main-test/introduction-to-algorithms/src/main/java/cn/net/bhe/introductiontoalgorithms/Keep.java
+++ java-main-test/introduction-to-algorithms/src/main/java/cn/net/bhe/introductiontoalgorithms/Keep.java
@@ -1,29 +1,9 @@
 package cn.net.bhe.introductiontoalgorithms;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.TreeMap;
-import java.util.TreeSet;
-
 public class Keep {
-    
-    public static void main(String[] args) {
-        System.out.println(trailingZeroes(10));
-    }
-    
-    public static int trailingZeroes(int n) {
-        int cnt5 = 0;
-        int cnt2 = 0;
-        while (n > 1) {
-            cnt5 += n / 5;
-            cnt2 += n / 2;
-            n--;
-        }
-        return Math.min(cnt2, cnt5);
-    }
-    
-}
 
+    public static void main(String[] args) {
+       
+    }
+
+}

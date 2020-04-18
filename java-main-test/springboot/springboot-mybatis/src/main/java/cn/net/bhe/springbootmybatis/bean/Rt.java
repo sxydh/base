25fae986f797c0534d7a48a3efc53f9b423a9235
sxydh <sxydhgg@gmail.com>
2020-04-18 09:02:54 +0800

    build

--- java-main-test/springboot/springboot-mybatis/src/main/java/cn/net/bhe/springbootmybatis/bean/Rt.java
+++ java-main-test/springboot/springboot-mybatis/src/main/java/cn/net/bhe/springbootmybatis/bean/Rt.java
@@ -1,8 +1,5 @@
 package cn.net.bhe.springbootmybatis.bean;
 
-import lombok.Data;
-
-@Data
 public class Rt {
 
     public static final int CODE_SUC = 200;
@@ -17,6 +14,30 @@ public class Rt {
 
     public static Rt instance() {
         return new Rt();
+    }
+
+    public Integer getSc() {
+        return sc;
+    }
+
+    public void setSc(Integer sc) {
+        this.sc = sc;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
     }
 
 }

aed87078210abd811d310de0bf41d3b3803cc097
sxydh <sxydhgg@gmail.com>
2020-04-20 09:59:05 +0800

    build

---
+++ java-main-test/springboot/springboot-mybatis/src/main/java/cn/net/bhe/springbootmybatis/listener/ApplicationStartup.java
@@ -0,0 +1,19 @@
+package cn.net.bhe.springbootmybatis.listener;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationStartup implements ApplicationListener<ApplicationReadyEvent> {
+
+    /**
+     * This event is executed as late as conceivably possible to indicate that
+     * the application is ready to service requests.
+     */
+    @Override
+    public void onApplicationEvent(final ApplicationReadyEvent event) {
+        // here your code ...
+        return;
+    }
+}

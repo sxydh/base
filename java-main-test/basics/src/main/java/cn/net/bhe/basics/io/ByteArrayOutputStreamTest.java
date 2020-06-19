2ac013ceb73420f53976bef5cba24fd88385a255
sxydh <sxydhgg@gmail.com>
2020-06-19 18:40:25 +0800

    build

---
+++ java-main-test/basics/src/main/java/cn/net/bhe/basics/io/ByteArrayOutputStreamTest.java
@@ -0,0 +1,33 @@
+package cn.net.bhe.basics.io;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ByteArrayOutputStreamTest {
+    
+    static Logger log = LoggerFactory.getLogger(ByteArrayOutputStreamTest.class);
+
+    /**
+     * 适用于特定场景下获取字节数组
+     */
+    @Test
+    public void toByteArray() {
+        try {
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            workbook.write(bos);
+            workbook.close();
+            byte[] bytes = bos.toByteArray();
+            bos.close();
+            log.info(Arrays.toString(bytes));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } 
+    }
+    
+}

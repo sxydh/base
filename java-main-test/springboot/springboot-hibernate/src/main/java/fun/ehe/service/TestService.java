26b3a4b511094dc97064d995023660e6b06d2af9
sxydh <sxydhgg@gmail.com>
2020-10-07 23:20:51 +0800

    build

--- java-main-test/springboot/springboot-hibernate/src/main/java/fun/ehe/service/TestService.java
+++ java-main-test/springboot/springboot-hibernate/src/main/java/fun/ehe/service/TestService.java
@@ -5,6 +5,9 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.event.SpringApplicationEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,6 +35,16 @@ public class TestService {
     @Transactional
     public int delete(int id) {
     	return testDao.delete(id);
+    }
+    
+    /**
+     * 初始化获取事务测试
+     * @param event
+     */
+    @EventListener(classes = {ApplicationReadyEvent.class}) 
+    @Transactional // 注意public修饰符
+    public void initAfterApplicationReady(SpringApplicationEvent event) {
+        delete(1);
     }
 
 }

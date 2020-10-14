aadc20650c934d0f30892e136fb0ba4c7d958c30
sxydh <sxydhgg@gmail.com>
2020-10-14 21:57:36 +0800

    build

---
+++ java-main-test/springboot/springboot-activiti/src/main/java/cn/net/bhe/springbootactiviti/SpringBootActiviti.java
@@ -0,0 +1,13 @@
+package cn.net.bhe.springbootactiviti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActiviti {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootActiviti.class, args);
+    }
+
+}

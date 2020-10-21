3921223552fd98412f23ebeaf797da220e1aba11
sxydh <sxydhgg@gmail.com>
2020-10-21 15:42:31 +0800

    build

--- java-main-test/springboot/springboot-mybatis/src/main/resources/table.sql
+++ java-main-test/springboot/springboot-mybatis/src/main/resources/table.sql
@@ -15,8 +15,22 @@ CREATE TABLE "BHE"."T_LOGBACK" (
   "POS" VARCHAR2(1000 BYTE),
   "THROWABLE" CLOB,
   "CALLER" VARCHAR2(3000 BYTE),
-  "MESSAGE" VARCHAR2(3000 BYTE),
+  "MESSAGE" CLOB,
   "THREAD" VARCHAR2(200 BYTE)
 )
 TABLESPACE "BHE";
-
+-- Add comments to the columns 
+comment on column BHE.T_LOGBACK.logdate
+  is '时间';
+comment on column BHE.T_LOGBACK.priority
+  is '级别';
+comment on column BHE.T_LOGBACK.pos
+  is '路径';
+comment on column BHE.T_LOGBACK.throwable
+  is '异常stack trace';
+comment on column BHE.T_LOGBACK.caller
+  is '调用stack trace';
+comment on column BHE.T_LOGBACK.message
+  is '内容message';
+comment on column BHE.T_LOGBACK.thread
+  is '线程';
